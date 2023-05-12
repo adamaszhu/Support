2b1ef22e1bd816be05ddca36b9d61b8bfc686079
Adamas Zhu <account@adamaszhu.com>
2023-05-12 21:32:28 +1000

    Add coordinator template

---
+++ Templates/Templates/Archirecture/ViewModel.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,14 @@
+/// ___FILENAME___
+///
+/// - version:
+/// - date: ___DATE___
+/// - author: ___FULLUSERNAME___
+protocol ___FILEBASENAME___Type: ViewModelType {
+}
+
+final class ___FILEBASENAME___: ViewModel<<#CoordinatorType#>>, ___FILEBASENAME___Type {
+
+    init(coordinator: <#CoordinatorType#>) {
+        super.init(coordinator: coordinator)
+    }
+}
