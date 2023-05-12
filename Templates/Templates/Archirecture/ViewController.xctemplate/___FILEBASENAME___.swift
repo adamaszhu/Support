e46acbe28f3169f6174144771b02d3e809fa37cb
Adamas Zhu <account@adamaszhu.com>
2023-05-12 21:23:06 +1000

    Add the view controller template

---
+++ Templates/Templates/Archirecture/ViewController.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,17 @@
+/// ___FILENAME___
+///
+/// - version:
+/// - date: ___DATE___
+/// - author: ___FULLUSERNAME___
+final class ___FILEBASENAME___: ViewController<<#ViewModelType#>> {
+    
+    func inject(coordinator: <#CoordinatorType#>) {
+        viewModel = <#ViewModel#>(coordinator: coordinator)
+    }
+    
+    override func initialize(with viewModel: any <#ViewModelType#>) {
+        <#Initialize the view#>
+    }
+}
+
+import UIKit
